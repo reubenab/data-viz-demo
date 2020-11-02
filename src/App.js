d27946e4aa39ba234e0e847bc306e03ab7e12323
Reuben Abraham <rabraham@nerdwallet.com>
2020-11-02 11:23:03 -0800

    Add header and body text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <header className="App-header">
+          <h1>
+            Comparing pay data
+          </h1>
+        </header>
+        <body className="App-body">
+          <p>
+            TODO: add the charts representing pay data here
+          </p>
+        </body>
+      </Container>
+    </>
   );
 }
 
