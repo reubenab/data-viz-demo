72f09d5239b2ff78ce356c942eb393942a166c22
Reuben Abraham <rabraham@nerdwallet.com>
2020-11-02 19:30:30 -0800

    Implement additional filters

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,4 +1,6 @@
 import { useMemo, useState } from 'react';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 import Container from 'react-bootstrap/Container';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -38,11 +40,10 @@ const salaryForEmployeeThisYear = (eid) => {
   return salaryData.filter(item => item.eid === eid && item.date.substring(6) === '2020');
 }
 
-const getAverageSalary = rawData => {
-  const numEmployees = _.size(rawData);
-  const allSalaries = _.flatten(_.map(rawData, ({ eid }) => salaryForEmployeeThisYear(eid)));
+const getAverageSalary = (employeeData) => {
+  const allSalaries = _.flatten(_.map(employeeData, ({ eid }) => salaryForEmployeeThisYear(eid)));
   const totalSalary = _.sumBy(allSalaries, ({ salary}) => parseInt(salary));
-  return _.round(totalSalary / numEmployees);
+  return _.round(totalSalary / (_.size(employeeData) || 1));
 }
 
 const COMPARE_KEYS = {
@@ -54,26 +55,70 @@ const COMPARE_KEYS = {
 
 const ADDITIONAL_FILTERS = {
   [COMPARE_KEYS.GENDER]: {
+    ALL: 'All',
     M: 'M',
     F: 'F',
   },
   [COMPARE_KEYS.DEPARTMENT]: {
-    SG_AND_A: 'SG&A',
+    ALL: 'All',
+    DESIGN: 'Design',
     ENGINEERING: 'Engineering',
+    MARKETING_AND_SALES: 'Marketing & Sales',
+    SG_AND_A: 'SG&A',
+  },
+  [COMPARE_KEYS.PERFORMANCE]: {
+    ALL: 'All',
+    EXCEEDS: 'Exceeds',
+    IMPROVEMENT_NEEDED: 'Improvement needed',
+    MEETS: 'Meets',
+    REDEFINES: 'Redefines',
+  },
+  [COMPARE_KEYS.JOB_CODE]: {
+    ALL: 'All',
+    R1: 'R1',
+    R2: 'R2',
+    R3: 'R3',
+    R4: 'R4',
+    R5: 'R5',
+    R6: 'R6',
+    R7: 'R7',
+    R8: 'R8',
+    R9: 'R9',
+    R10: 'R10',
   },
 }
 
+const initialFilter = {
+  [COMPARE_KEYS.GENDER]: ADDITIONAL_FILTERS[COMPARE_KEYS.GENDER].ALL,
+  [COMPARE_KEYS.DEPARTMENT]: ADDITIONAL_FILTERS[COMPARE_KEYS.DEPARTMENT].ALL,
+  [COMPARE_KEYS.PERFORMANCE]: ADDITIONAL_FILTERS[COMPARE_KEYS.PERFORMANCE].ALL,
+  [COMPARE_KEYS.JOB_CODE]: ADDITIONAL_FILTERS[COMPARE_KEYS.JOB_CODE].ALL,
+};
+
 const Body = () => {
   const [compareValue, setCompareValue] = useState(COMPARE_KEYS.GENDER);
+  const [additionalFilters, setAdditionalFilters] = useState(initialFilter);
   const rawEmployeeData = useMemo(() => getEmployeesRaw(), []);
   const groupedData = useMemo(() => _.groupBy(rawEmployeeData, compareValue), [rawEmployeeData, compareValue]);
-  const unsortedData = _.map(groupedData, (groupEmployeeData, groupName) => ({
-    groupName,
-    averageSalary: getAverageSalary(groupEmployeeData),
-  }));
+  const unsortedData = _.map(groupedData, (groupEmployeeData, groupName) => {
+    const filteredEmployeeData = _.filter(groupEmployeeData, (employee) => {
+      return _.every(additionalFilters, (filterValue, filterType) => {
+        if (filterValue === 'All') {
+          return true;
+        }
+        return employee[filterType] === filterValue;
+      })
+    });
+    return {
+      groupName,
+      averageSalary: getAverageSalary(filteredEmployeeData),
+      numEmployees: _.size(filteredEmployeeData),
+    };
+  });
   const data = _.sortBy(unsortedData, 'groupName');
   const handleCompareButtonSelect = (eventKey) => {
     setCompareValue(eventKey);
+    setAdditionalFilters(initialFilter);
   }
   return (
     <Container>
@@ -85,6 +130,41 @@ const Body = () => {
           ))}
         </DropdownButton>
       </Row>
+      <br />
+      <Row>Additional filters</Row>
+      {_.map(ADDITIONAL_FILTERS, (subgroups, groupName) => {
+        if (groupName === compareValue) {
+          return null;
+        }
+        return (
+          <Row className="filter-label">
+            {groupName}:
+            <ButtonGroup toggle>
+              {_.map(subgroups, (subgroupName, subgroupKey) => {
+                const handleChangeFilterRadio = (e) => {
+                  setAdditionalFilters({
+                    ...additionalFilters,
+                    [groupName]: e.currentTarget.value,
+                  });
+                };
+                return (
+                  <ToggleButton
+                    key={`${groupName}-${subgroupName}`}
+                    type="radio"
+                    variant="secondary"
+                    name="radio"
+                    value={subgroupName}
+                    checked={additionalFilters[groupName] === ADDITIONAL_FILTERS[groupName][subgroupKey]}
+                    onChange={handleChangeFilterRadio}
+                  >
+                    {subgroupName}
+                  </ToggleButton>
+                );
+              })}
+            </ButtonGroup>
+          </Row>
+        );
+      })}
       <Row>
         <DataVizCharts data={data} label={_.upperFirst(compareValue)} />
       </Row>
@@ -95,15 +175,17 @@ const Body = () => {
               {_.map(COMPARE_KEYS, (val) => (
                 <th>{val}</th>
               ))}
-              <th>Salary</th>
+              <th># employees</th>
+              <th>Avg. salary</th>
             </tr>
           </thead>
           <tbody>
-            {_.map(data, ({ groupName, averageSalary }) => (
+            {_.map(data, ({ groupName, averageSalary, numEmployees }) => (
               <tr>
                 {_.map(COMPARE_KEYS, (val) => (
-                  <td>{compareValue === val ? groupName : 'All'}</td>
+                  <td>{compareValue === val ? groupName : additionalFilters[val]}</td>
                 ))}
+                <td>{numEmployees}</td>
                 <td>{`$${averageSalary}`}</td>
               </tr>
             ))}
